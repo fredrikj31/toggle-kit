8dc21d7ac320a2c62e5d9475e5928acedee17f03
Fredrik Johansen <fredrik1206@gmail.com>
2025-07-24 21:23:16 +0200

    fix(helpers): fixes type in defineFlags helper function
    fixes generic type in defineFlags helper function, to fix auto-complete when using defineFlags function

--- src/helpers/defineFlags.ts
+++ src/helpers/defineFlags.ts
@@ -1,8 +1,8 @@
 import { FeatureFlag, User } from "../types/types";
 
-export function defineFlags<const TUser extends User>(
-  _user: TUser,
-  flags: readonly FeatureFlag<keyof TUser & string>[]
-) {
+export function defineFlags<
+  const TUser extends User,
+  const TFlags extends readonly FeatureFlag<keyof TUser & string>[]
+>(_user: TUser, flags: TFlags) {
   return flags;
 }
