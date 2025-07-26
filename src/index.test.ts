3b0bfdea10d254061151be87564a573f3b49926f
Fredrik Johansen <fredrik1206@gmail.com>
2025-07-26 11:55:22 +0200

    test(client): adds unit tests for string, number & boolean flags
    adds separate unit tests for string, number & boolean feature flags

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,23 +2,60 @@ import { createFeatureFlagClient } from ".";
 import { defineFlags } from "./helpers/defineFlags";
 
 describe("End-To-End Tests - Library Tests", () => {
-  it("should return true when getting isEnabled feature flag", () => {
-    const user = {
-      email: "test@example.com",
-    } as const;
-    const flags = defineFlags(user, [
-      {
-        name: "admin-dashboard",
-        condition: {
-          type: "equal",
-          attribute: "email",
-          value: "test@example.com",
-        },
+  it("should return true when feature flag is string", () => {
+    const client = createFeatureFlagClient({
+      user: {
+        email: "test@example.com",
       },
-    ]);
+      flags: [
+        {
+          name: "secret-page",
+          condition: {
+            type: "equal",
+            attribute: "email",
+            value: "test@example.com",
+          },
+        },
+      ],
+    });
+    expect(client.isEnabled("secret-page")).toBe(true);
+  });
 
-    const client = createFeatureFlagClient({ user, flags });
+  it("should return true when feature flag is number", () => {
+    const client = createFeatureFlagClient({
+      user: {
+        price: 100,
+      },
+      flags: [
+        {
+          name: "discount-price",
+          condition: {
+            type: "equal",
+            attribute: "price",
+            value: 100,
+          },
+        },
+      ],
+    });
+    expect(client.isEnabled("discount-price")).toBe(true);
+  });
 
+  it("should return true when feature flag is boolean", () => {
+    const client = createFeatureFlagClient({
+      user: {
+        isAdmin: true,
+      },
+      flags: [
+        {
+          name: "admin-dashboard",
+          condition: {
+            type: "equal",
+            attribute: "isAdmin",
+            value: true,
+          },
+        },
+      ],
+    });
     expect(client.isEnabled("admin-dashboard")).toBe(true);
   });
 });
