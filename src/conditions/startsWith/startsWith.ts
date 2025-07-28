9e32a6d7182c6ad0194cd6c8c368b9a70293a827
Fredrik Johansen <fredrik1206@gmail.com>
2025-07-28 20:01:48 +0200

    feat(conditions): adds "startsWith" condition function

---
+++ src/conditions/startsWith/startsWith.ts
@@ -0,0 +1,15 @@
+export const startsWithCondition = ({
+  userValue,
+  value,
+}: {
+  userValue: string | number | boolean;
+  value: string;
+}): boolean => {
+  if (typeof userValue === "number" || typeof userValue === "boolean") {
+    return false;
+  }
+
+  const regex = RegExp(`^${value}`, "gm");
+
+  return regex.test(userValue);
+};
