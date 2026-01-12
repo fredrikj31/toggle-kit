37d997b00f87c60b67a16ad3cc12c57c24593b94
Fredrik Johansen <fredrik1206@gmail.com>
2026-01-12 20:14:06 +0100

    test(helpers): adds test case for unsupported condition type
    adds special unit test to make sure unsupported condition type does not fail whole flag (other conditions)

--- src/index.test.ts
+++ src/index.test.ts
@@ -90,4 +90,40 @@ describe("End-To-End Tests - Library Tests", () => {
     });
     expect(client.isEnabled("admin-dashboard")).toBe(true);
   });
+
+  it("should continue and check other conditions, when condition type is not supported", () => {
+    // This is a little special test case
+    // We want to make sure that even if one of the conditions is unsupported, it does not fail the whole flag (other conditions)
+    // We simply just continue through the other conditions, and check if some of them return true.
+    const isAdminCondition = {
+      type: "hej" as const,
+      attribute: "isAdmin",
+      expectedValue: true,
+    };
+    const roleCondition = {
+      type: "contains" as const,
+      attribute: "roles",
+      expectedValue: "admin",
+    };
+
+    const client = createFeatureFlagClient({
+      property: {
+        isAdmin: false,
+        roles: "moderator,admin",
+      },
+      flags: [
+        {
+          name: "admin-dashboard",
+          conditions: [
+            // It's giving a Type error, when the type is not supported. We ignore it here, because we want to test what happens when we give it an unsupported condition type.
+            // @ts-ignore
+            isAdminCondition,
+            roleCondition, // This should make sure the flag return true, even though the "isAdminCondition" is unsupported.
+          ],
+        },
+      ],
+    });
+
+    expect(client.isEnabled("admin-dashboard")).toBe(true);
+  });
 });
